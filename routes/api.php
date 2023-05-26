a1954c8ced6005b94bebd85429edfe898e4990d0
aldhi saputra <aldhisaputra795@gmail.com>
2023-05-26 22:18:54 +0700

    update API dan Route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\FoodController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,3 +26,5 @@ Route::post('logout', [UserController::class, 'logout']);
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
+
+route::get('food', [FoodController::class, 'all']);
