2426c2317ac4746b30e4a05267de730e3d0de7f6
aldhi saputra <aldhisaputra795@gmail.com>
2023-06-02 21:21:07 +0700

    update

--- app/Http/Controllers/API/MidtransController.php
+++ app/Http/Controllers/API/MidtransController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Midtrans\Config;
+use Midtrans\Notification as MidtransNotification;
+
 
 class MidtransController extends Controller
 {
-    //
+    public function callback(Request $request)
+    {
+        //set konfigurasi midtrans
+        Config::$serverKey = config('service.midtrans.serverKey');
+        config::$isProduction = config('service.midtrans.isProduction');
+        config::$isSanitized = config('service.midtrans.isSanitized');
+        config::$is3ds = config('service.midtrans.is3ds');
+
+        //Buat instance midtrans notifikasi
+        $notification = new MidtransNotification();
+    }
 }
