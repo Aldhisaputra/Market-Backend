2c5d1d9021d68e7809199528aa3b5e9319d3a4c9
aldhi saputra <aldhisaputra795@gmail.com>
2023-06-02 21:52:12 +0700

    midtrans

--- app/Http/Controllers/API/MidtransController.php
+++ app/Http/Controllers/API/MidtransController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\transaction;
 use Illuminate\Http\Request;
 use Midtrans\Config;
 use Midtrans\Notification as MidtransNotification;
@@ -20,5 +21,54 @@ class MidtransController extends Controller
 
         //Buat instance midtrans notifikasi
         $notification = new MidtransNotification();
+
+        //Assign ke variabel untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+        //Cari transaksi berdasarkan ID
+        $transaction = transaction::findOrFail($order_id);
+
+        //hansle notifikasi status midtran
+        if($status == 'capture')
+        {
+            if($type == 'credit_card')
+            {
+                if($fraud == 'challenge')
+                {
+                    $transaction->status = 'PENDING';
+                }
+                else
+                {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        }
+        else if($status == 'settlement')
+        {
+            $transaction->status = 'SUCCESS';
+        }
+        else if($status == 'pending')
+        {
+            $transaction->status = 'PENDING';
+        }
+        else if($status == 'deny')
+        {
+            $transaction->status = 'CANCELLED';
+        }
+        else if($status == 'expire')
+        {
+            $transaction->status = 'CANCELLED';
+        }
+        else if($status == 'cancel')
+        {
+            $transaction->status = 'CANCELLED';
+        }
+        
+        // Simpan Transaksi
+        $transaction ->save();
+
     }
 }
